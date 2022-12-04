fed20e751cfeaa4d1513574c43dd861c7b77305c
Dan Ridzoň <ridzodan@fit.cvut.cz>
2022-12-04 06:25:52 +0100

    Added day 4 solution.

---
+++ day4/solver.py
@@ -0,0 +1,21 @@
+import re
+
+# Load the file
+input_file = open('input_1.txt', 'r')
+lines = input_file.readlines()
+
+count = 0
+
+for line in lines:
+
+    # Convert input to the list of ints.
+    all = re.split("[,\-\n]",line.strip())
+    all = [int(i) for i in all]
+
+    # Check if ranges overlap.
+    if all[0] <= all[2] and all[1] >= all[3]:
+        count += 1
+    elif all[2] <= all[0] and all[3] >= all[1]:
+        count += 1
+
+print(count)
---
+++ day_/solver.py
@@ -0,0 +1,6 @@
+# Load the file
+input_file = open('input_1.txt', 'r')
+lines = input_file.readlines()
+
+for line in lines:
+    line = line.strip()
