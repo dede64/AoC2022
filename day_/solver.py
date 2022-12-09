416f577c187602df0c97155935766433ff7a5a17
Dan Ridzoň <ridzodan@fit.cvut.cz>
2022-12-09 07:25:04 +0100

    Updated day prefab.

--- day_/solver.py
+++ day_/solver.py
@@ -1,17 +1,32 @@
 import re
 
 # Load the file
-input_file = open('input_1.txt', 'r')
+input_file = open('dayX/input_1.txt', 'r')
 lines = input_file.readlines()
 
 result = 0
 
+#########################
+# Regex split.
 for line in lines:
 
     # Apply split regex on the line.
     data = re.split("[,\-\n]",line.strip())
 
-    ....
+########################
+# Load 2D area.
+area = []
+for line in lines:
+
+    line = line.strip()
+    row = []
+
+    for ch in line: 
+        row.append(ch)
+    
+    area.append(row)
+
+    
 
 
-return result
+print(result)
